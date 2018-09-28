4d91233bec83544e80b25617b480170d299060e8
David Castaneda <dcast188@fiu.edu>
2018-09-28 01:59:00 -0400

    rm tmp/

--- api/src/index.js
+++ api/src/index.js
@@ -1,21 +1,21 @@
-import "./env"
+import "./env";
 import express from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import cors from "cors";
 import { apiRouter } from "./routes";
-import  db  from "./db";
+import db from "./db";
 
 const app = express();
 
 const { PORT } = process.env;
 
-db()
+db();
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", apiRouter);
 
-app.listen(PORT, console.log(`> Listening ${PORT}`));
+app.listen(PORT, console.log("> API Listening"));
